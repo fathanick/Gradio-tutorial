3ea1b74ad00b15ca4265c2f7e2f36c42ec5a81f4
Fathan <fathanel@gmail.com>
2024-05-18 00:09:12 +0700

    add image classification

---
+++ image_classification/app_image_cls.py
@@ -0,0 +1,20 @@
+import gradio as gr
+from transformers import ViTImageProcessor, ViTForImageClassification
+from PIL import Image
+import requests
+
+url = 'http://images.cocodataset.org/val2017/000000039769.jpg'
+image = Image.open(requests.get(url, stream=True).raw)
+
+processor = ViTImageProcessor.from_pretrained('google/vit-base-patch16-224')
+model = ViTForImageClassification.from_pretrained('google/vit-base-patch16-224')
+
+def predict(image):
+    inputs = processor(images=image, return_tensors="pt")
+    outputs = model(**inputs)
+    logits = outputs.logits
+    predicted_class_idx = logits.argmax(-1).item()
+    return model.config.id2label[predicted_class_idx]
+
+demo = gr.Interface(fn=predict, inputs="image", outputs="text")
+demo.launch()
